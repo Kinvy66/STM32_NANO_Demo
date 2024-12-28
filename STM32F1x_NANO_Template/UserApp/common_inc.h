dcc920a5f6d75314b4d17348a0e54823f4103eef
Kinvy <kinvy66@163.com>
2024-12-28 20:51:19 +0800

    +#; 添加目标工程

---
+++ STM32F1x_NANO_Template/UserApp/common_inc.h
@@ -0,0 +1,18 @@
+/**
+* @file common_inc.h
+* @author Kinvy
+* @email kinvy66@163.com
+* @date: 2024/12/28 
+* @description: 
+**/
+
+#ifndef STM32F1X_NANO_TEMPLATE_COMMON_INC_H
+#define STM32F1X_NANO_TEMPLATE_COMMON_INC_H
+
+#include "led.h"
+
+void user_main();
+void bsp_init();
+
+
+#endif //STM32F1X_NANO_TEMPLATE_COMMON_INC_H
