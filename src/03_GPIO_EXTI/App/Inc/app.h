b8c33c5bc224caf6a12273cdb0e2479a170689ae
Kinvy <kinvy66@163.com>
2025-02-22 18:54:09 +0800

    按键外部中断实验

---
+++ src/03_GPIO_EXTI/App/Inc/app.h
@@ -0,0 +1,15 @@
+/**
+* @file app.h
+* @author Kinvy
+* @email kinvy66@163.com
+* @date: 2025/2/16 
+* @description: 
+**/
+
+#ifndef INC_00_TEMPLATE_APP_H
+#define INC_00_TEMPLATE_APP_H
+
+void hw_init(void);
+int app(void);
+
+#endif //INC_00_TEMPLATE_APP_H
---
+++ src/03_GPIO_EXTI/App/Src/app.c
@@ -0,0 +1,31 @@
+/**
+* @file app.c
+* @author Kinvy
+* @email kinvy66@163.com
+* @date: 2025/2/16 21:13
+* @description: 
+**/
+
+#include "app.h"
+#include "stm32f1xx_hal.h"
+#include "main.h"
+#include "led.h"
+#include "beep.h"
+#include "key.h"
+
+/**
+ * @brief Hardware Init
+ */
+void hw_init(void) {
+    LED0_OFF;
+    LED1_OFF;
+    BEEP_ON;
+}
+
+/**
+ * @brief Application Entry
+ * @return
+ */
+int app(void) {
+    return 0;
+}
