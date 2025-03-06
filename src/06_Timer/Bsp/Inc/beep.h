53ce1347045ec1afe3b8256186ff2a439cf006e6
Kinvy <kinvy66@163.com>
2025-03-06 21:27:23 +0800

    定时器中断

---
+++ src/06_Timer/Bsp/Inc/beep.h
@@ -0,0 +1,16 @@
+/**
+* @file beep.h
+* @author Kinvy
+* @email kinvy66@163.com
+* @date: 2025/2/22 
+* @description: 
+**/
+
+#ifndef __BEEP_H
+#define __BEEP_H
+#include "main.h"
+
+#define BEEP_ON()     HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, GPIO_PIN_RESET)
+#define BEEP_OFF()    HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, GPIO_PIN_SET)
+
+#endif //__BEEP_H
---
+++ src/06_Timer/Bsp/Src/beep.c
@@ -0,0 +1,7 @@
+/**
+* @file beep.c
+* @author Kinvy
+* @email kinvy66@163.com
+* @date: 2025/2/22 17:19
+* @description: 
+**/
